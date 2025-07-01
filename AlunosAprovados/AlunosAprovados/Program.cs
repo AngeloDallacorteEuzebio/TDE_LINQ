ac4ffe82df43964dcf29a7696e9f17594a4ae146
AngeloDallacorteEuzebio <imprimir.imbe@gmail.com>
2025-07-01 06:58:02 -0300

    Adiciona os arquivos atualizados

---
+++ AlunosAprovados/AlunosAprovados/Program.cs
@@ -0,0 +1,21 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        Dictionary<string, double> alunos = new Dictionary<string, double>();
+
+        alunos.Add("Lucas", 10);
+        alunos.Add("Ana", 5.0);
+        alunos.Add("Pedro", 10);
+        alunos.Add("Joana", 5.0);
+
+        var aprovados = alunos.Where(a => a.Value >= 7.0);
+
+        Console.WriteLine("Alunos aprovados:");
+        foreach (var aluno in aprovados)
+        {
+            Console.WriteLine(aluno.Key);
+        }
+    }
+}
---
+++ FiladeAtendimento/FiladeAtendimento/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Dictionary<int, string> clientes = new Dictionary<int, string>();
+
+        clientes.Add(1, "Lucas");
+        clientes.Add(2, "Carla");
+        clientes.Add(3, "João");
+        clientes.Add(4, "Marcos");
+        clientes.Add(5, "Juliana");
+
+        var clientesFiltrados = clientes.Where(c => c.Key >= 3);
+
+        Console.WriteLine("Clientes filtrados (ID >= 3):");
+        foreach (var cliente in clientesFiltrados)
+        {
+            Console.WriteLine($"ID: {cliente.Key}, Nome: {cliente.Value}");
+        }
+
+        Console.WriteLine();
+
+        Queue<string> filaAtendimento = new Queue<string>();
+
+        filaAtendimento.Enqueue("Lucas");
+        filaAtendimento.Enqueue("Carla");
+        filaAtendimento.Enqueue("João");
+        filaAtendimento.Enqueue("Marcos");
+        filaAtendimento.Enqueue("Juliana");
+
+        Console.WriteLine("Iniciando atendimento:");
+        while (filaAtendimento.Count > 0)
+        {
+            Console.WriteLine($"Atendendo cliente: {filaAtendimento.Dequeue()}");
+        }
+        Console.WriteLine("Atendimento encerrado. ");
+    }
+}
